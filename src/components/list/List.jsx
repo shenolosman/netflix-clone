35aa9651df1547d669b8223c08210ddbbb9a8ef6
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-10-24 23:22:32 +0200

    listing items

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
-import "./List.scss"
+import React, { useRef, useState } from "react";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import "./List.scss";
+import ListItem from "../listItem/ListItem";
 const List = () => {
-    return (
-        <div>
-            List
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [isMoved, setIsMoved] = useState(false);
+
+  //like in vanillaJS to get by classname instead of same method in react
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    //to calculate items position on the screen
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 3) {
+      setSlideNumber.apply(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+  return (
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosIcon
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          
         </div>
-    );
-}
+        <ArrowForwardIosIcon className="sliderArrow right" onClick={() => handleClick("right")} />
+      </div>
+    </div>
+  );
+};
 
 export default List;
---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./ListItem.scss"
+const ListItem = () => {
+    return (
+        <div className='listItem'>
+            List item
+        </div>
+    );
+}
+
+export default ListItem;
