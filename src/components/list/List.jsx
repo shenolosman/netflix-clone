f4b76b884a2742d067f10299e82b51f7f660814c
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-10-27 22:46:15 +0200

    list item popup menu

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -17,10 +17,12 @@ const List = () => {
     if (direction === "left" && slideNumber > 0) {
       setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      // console.log("slideNumber :",slideNumber)
     }
     if (direction === "right" && slideNumber < 3) {
-      setSlideNumber.apply(slideNumber + 1);
+      setSlideNumber(slideNumber + 1);
       listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      // console.log("slideNumber :",slideNumber)
     }
   };
   return (
@@ -33,17 +35,28 @@ const List = () => {
           style={{ display: !isMoved && "none" }}
         />
         <div className="container" ref={listRef}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
+        </div>
+        <div className="container" ref={listRef}>
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
+          <ListItem index={9} />
         </div>
         <ArrowForwardIosIcon className="sliderArrow right" onClick={() => handleClick("right")} />
       </div>
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
-import "./ListItem.scss"
-const ListItem = () => {
-    return (
-        <div className='listItem'>
-            List item
-        </div>
-    );
-}
+import React, { useState } from "react";
+import "./ListItem.scss";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import AddIcon from "@mui/icons-material/Add";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import ThumbDownOffAltIcon from "@mui/icons-material/ThumbDownOffAlt";
+import "./ListItem.scss";
+const ListItem = ({ index }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const trailer = "https://joy.videvo.net/videvo_files/video/free/2012-09/large_watermarked/hd0456_preview.mp4";
+  return (
+    <div
+      className="listItem"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+    >
+      <img src="https://placeimg.com/640/480/tech/sepia" alt="" />
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrowIcon className="icon"/>
+              <AddIcon className="icon"/>
+              <ThumbUpOffAltIcon className="icon"/>
+              <ThumbDownOffAltIcon className="icon"/>
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>1999</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem minus voluptates aspernatur, possimus voluptas
+              doloribus sint magni hic sapiente inventore.
+            </div>
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
+      
+    </div>
+  );
+};
 
 export default ListItem;
