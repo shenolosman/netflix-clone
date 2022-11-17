6f6792623c8e49283620144adc4f1182492fbd68
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-11-17 19:34:03 +0100

    login on api

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,11 @@
-const router = require('express').Router()
-const User=require("../models/User")
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJs = require("crypto-js");
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.email,
+    password: CryptoJs.AES.encrypt(req.body.email, process.env.SECRET_KEY).toString(),
   });
   try {
     const user = await newUser.save();
@@ -14,4 +15,21 @@ router.post("/register", async (req, res) => {
   }
 });
 
-module.exports= router;
+//Login
+router.post("login", async (res, req) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong pass or username!");
+
+    const bytes = CryptoJs.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPass = bytes.toString(CryptoJs.enc.Utf8);
+
+    originalPass !== req.body.password && res.status(401).json("Wrong pass or username!");
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
