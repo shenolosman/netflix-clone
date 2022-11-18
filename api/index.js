db412f0027fe74a71ec43665a3c4dcdb4bc37c11
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-11-18 13:44:43 +0100

    update user api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const routerAuth = require("./routes/auth");
+const routerUser=require("./routes/users")
 // const cors = require("cors");
 const app = express();
 dotenv.config();
@@ -21,7 +22,8 @@ app.use(express.json());
 app.get("/", (req,res) => {
   res.json({ message: `API Working! Auth Routes: '/api/auth/register' , '/api/auth/login'` });
 });
-app.use("/api/auth", routerAuth); //with this route adding in authroute afterwards
+app.use("/api/auth", routerAuth); 
+app.use("/api/users", routerUser); 
 
 app.listen(8800, () => {
   console.log("backend server is running!");
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,14 +1,36 @@
-const router =require("express").Router()
-const User=require("../models/User")
+const router = require("express").Router();
+const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const verify = require("../verifyToken");
 
 //Update
-
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString();
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can update only your account!");
+  }
+});
 //Delete
 
 //Get 1 user
 
 //Get all for admin
 
-
 //Get user stats for admin
+
+module.exports = router;
