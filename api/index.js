d9707272e1574612e8576bb46ca3a36ef111b43f
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-11-18 15:34:06 +0100

    list of types api created

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const routerAuth = require("./routes/auth");
 const routerUser=require("./routes/users")
 const routerMovie=require("./routes/movies")
+const routerList=require("./routes/lists")
 // const cors = require("cors");
 const app = express();
 dotenv.config();
@@ -26,6 +27,7 @@ app.get("/", (req,res) => {
 app.use("/api/auth", routerAuth); 
 app.use("/api/users", routerUser); 
 app.use("/api/movies", routerMovie); 
+app.use("/api/lists", routerList); 
 
 app.listen(8800, () => {
   console.log("backend server is running!");
